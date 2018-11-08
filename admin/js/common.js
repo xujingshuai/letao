84ee8ebe4361503f478da3fda7ea31b027d7b036
xujingshuai <15128654212@163.com>
2018-11-08 22:36:23 +0800

    后台管理员

--- admin/js/common.js
+++ admin/js/common.js
@@ -1,8 +1,36 @@
 
-	NProgress.start();
+NProgress.start();
 
-	NProgress.done();
+NProgress.done();
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
+$('.navs ul').prev('a').on('click', function () {
+	$(this).next().slideToggle();
+});
+
+
+// 检测用户是否登录
+$.ajax({
+	url: '/api/employee/checkRootLogin',
+	type: 'get',
+	success: function (msg) {
+		// 未登录
+		if (msg.error) {
+			return location.href = '/admin/login.html?url=' + location.href;
+		}
+	}
+});
+
+
+// 退出登录
+$('.logout').on('click',function() {
+	// 发送请求
+	$.ajax({
+		url: '/api/employee/employeeLogout',
+		type: 'get',
+		success: function(msg) {
+			if(msg.success) {
+				location.href = '/admin/login.html';
+			}
+		}
 	});
+})
